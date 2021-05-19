e1837ec0ed24d7b67e456680909ada2eadfab30c
unknown <naveentruly86@gmail.com>
2021-05-19 17:52:27 +0530

    Added: Welcome Message

---
+++ src/main/java/com/linecomputation/Linecompare.java
@@ -0,0 +1,7 @@
+package com.linecomputation;
+
+public class Linecompare {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
