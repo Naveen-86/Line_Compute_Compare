cd5cfad997d62bc58ae471cd0a1bd37dd3e33415
unknown <naveentruly86@gmail.com>
2021-05-19 18:13:14 +0530

    Added: Checking Equality of Two lines

--- src/main/java/com/linecomputation/Linecompare.java
+++ src/main/java/com/linecomputation/Linecompare.java
@@ -6,6 +6,7 @@ public class Linecompare {
         System.out.println("Welcome to Line Comparison Computation Program");
 
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter First Line");
         System.out.println("Enter the First point Co-ordinates");
         int x1 = s.nextInt();
         int y1 = s.nextInt();
@@ -14,6 +15,19 @@ public class Linecompare {
         int x2 = s.nextInt();
         int y2 = s.nextInt();
         Double firstLine = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
-        System.out.println("Length of line : " + firstLine);
+        System.out.println("Length of First line : " + firstLine);
+
+        System.out.println("Enter Second Line");
+        System.out.println("Enter First point Co-ordinates");
+        int x3 = s.nextInt();
+        int y3 = s.nextInt();
+
+        System.out.println("Enter Second point Co-ordinates");
+        int x4 = s.nextInt();
+        int y4 = s.nextInt();
+        Double secondLine = Math.sqrt(Math.pow((x4 - x3),2) + Math.pow((y4 - y3),2));
+        System.out.println("Length of Second Line : " + secondLine);
+
+        System.out.println(firstLine.equals(secondLine));
     }
 }
